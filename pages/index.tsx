24a40a0a35bd0c2ec3379f8adaa77fe13ec63e45
Gustavo Santos <gefgu@hotmail.com>
2022-09-18 16:51:36 -0300

    Add quotes through SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+interface HomeProps {
+  id?: number;
+  advice?: string;
+}
+
+const Home: NextPage = ({ id, advice }: HomeProps) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,16 +17,27 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.quoteBox}>
-        <h2 className={styles.subHeading}>Advice #117</h2>
-        <h1 className={styles.quote}>
-          "It is easy to sit up and take notice, what's difficult is getting up
-          and taking action."
-        </h1>
+        <h2 className={styles.subHeading}>Advice #{id}</h2>
+        <h1 className={styles.quote}>"{advice}"</h1>
         <Image src="/pattern-divider-desktop.svg" height={16} width={444} />
-        <button className={styles.button}><Image src="/icon-dice.svg" width={24} height={24} layout="responsive"/></button>
+        <button className={styles.button}>
+          <Image
+            src="/icon-dice.svg"
+            width={24}
+            height={24}
+            layout="responsive"
+          />
+        </button>
       </main>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+
+  return { props: { ...data.slip } };
 };
 
 export default Home;
