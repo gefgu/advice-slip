c5d04c3ac57581e1ed9340afaaae70f145d45ca9
Gustavo Santos <gefgu@hotmail.com>
2022-09-18 15:52:41 -0300

    Add background styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css';
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Advice Slip</title>
         <link rel="icon" href="/favicon.png" />
@@ -13,10 +12,13 @@ const Home: NextPage = () => {
 
       <main>
         <h2>Advice #117</h2>
-        <h1>"It is easy to sit up and take notice, what's difficult is getting up and taking action."</h1>
+        <h1>
+          "It is easy to sit up and take notice, what's difficult is getting up
+          and taking action."
+        </h1>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
